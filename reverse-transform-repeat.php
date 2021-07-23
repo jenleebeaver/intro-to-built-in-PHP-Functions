079b41c6f5e9aa92742304c12fbb9d7844e05ab2
Jennifer Beaver <jen.lee.beaver@gmail.com>
2021-07-22 18:00:50 -0700

    Create reverse-transform-repeat.php

---
+++ reverse-transform-repeat.php
@@ -0,0 +1,22 @@
+//reverses string 
+echo strrev("Hello, World!"); // Prints: !dlroW ,olleH
+
+//strtolower():
+//transforms an argument string into lowercase letters 
+echo ("\n");
+echo strtolower("\nHeLLo"); // Prints: hello
+
+//str_repeat(string, integer):
+//string repeated the argument number of times 
+echo ("\n");
+echo str_repeat("\nhi", 10); 
+
+//PRACTICE 
+echo ("\n");
+echo strrev(".pu ti peeK .taerg gniod er'uoY\n");
+
+echo ("\n");
+echo strtolower("\n SOON, tHiS WILL Look NoRmAL.");
+
+echo ("\n");
+echo str_repeat("\nThere's no place like home.\n", 3);
